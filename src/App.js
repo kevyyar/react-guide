e5c99469163cad3599f13d87f4a275084d554257
Kevin Barreto <barretokevin@hotmail.com>
2022-01-18 12:25:08 -0600

    ADD functional components in same file

--- src/App.js
+++ src/App.js
@@ -6,28 +6,7 @@ import Componente from "./components/Componente";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <section>
-        <Componente msg={"hello im a component from a prop"} />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </section>
+      <Componente msg={"hello im a component from a prop"} />
     </div>
   );
 }
--- src/components/Componente.jsx
+++ src/components/Componente.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 
 // Class based component
+/*
 class Componente extends Component {
   render(props) {
     return <h1>{this.props.msg}</h1>;
   }
 }
+*/
+
+// Function based component
+// #1 function declaration
+/*
+function Componente(props) {
+  return <h1>{props.msg}</h1>;
+}
+ */
+//#2 Arrow Function
+const Componente = (props) => <h1>{props.msg}</h1>;
 
 export default Componente;
